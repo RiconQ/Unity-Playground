a660dd9e23690941fd21619f953b97e68a86197c
Ricon <64973117+RiconQ@users.noreply.github.com>
2024-11-04 22:19:07 +0900

    Basics - Compute Shader
    ~ 2.1 Drawing Many Meshes

--- Assets/Basics/04.Compute Shaders/Scripts/GPUGraph.cs
+++ Assets/Basics/04.Compute Shaders/Scripts/GPUGraph.cs
@@ -6,6 +6,11 @@ namespace Basics
 {
     public class GPUGraph : MonoBehaviour
     {
+        [SerializeField] private ComputeShader computeShader;
+
+        [SerializeField] private Material material;
+        [SerializeField] private Mesh mesh;
+
         [Range(10, 200)]
         [SerializeField] private int _resolution = 20;
 
@@ -24,6 +29,28 @@ namespace Basics
         private FunctionLibrary.EFunctionName _transitionFunction;
 
         private ComputeBuffer _positionsBuffer;
+
+
+        static readonly int
+            positionsId = Shader.PropertyToID("_Positions"),
+            resolutionId = Shader.PropertyToID("_Resolution"),
+            stepId = Shader.PropertyToID("_Step"),
+            timeId = Shader.PropertyToID("_Time");
+
+        private void UpdateFunctionOnGPU()
+        {
+            float step = 2f / _resolution;
+            computeShader.SetInt(resolutionId, _resolution);
+            computeShader.SetFloat(stepId, step);
+            computeShader.SetFloat(timeId, Time.time);
+            computeShader.SetBuffer(0, positionsId, _positionsBuffer);
+
+            int groups = Mathf.CeilToInt(_resolution / 8f);
+            computeShader.Dispatch(0, groups, groups, 1);
+
+            var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / _resolution));
+            Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, _positionsBuffer.count);
+        }
 
         private void Enable()
         {
@@ -55,6 +82,8 @@ namespace Basics
                 //_function = FunctionLibrary.GetNextFunctionName(_function);
                 PickNextFunction();
             }
+
+            UpdateFunctionOnGPU();
         }
 
         private void PickNextFunction()
