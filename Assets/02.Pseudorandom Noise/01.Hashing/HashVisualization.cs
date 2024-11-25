13a193d94d40aced0e536890247a8792e387b7a4
김지훈 <ricon4246@gmail.com>
2024-11-25 15:29:42 +0900

    Pseudorandom Noise - Hashing
    ~ 1.4 Patterns

--- Assets/02.Pseudorandom Noise/01.Hashing/HashVisualization.cs
+++ Assets/02.Pseudorandom Noise/01.Hashing/HashVisualization.cs
@@ -14,9 +14,14 @@ public class HashVisualization : MonoBehaviour
         [WriteOnly]
         public NativeArray<uint> hashes;
 
+        public int resolution;
+        public float invResolution;
+
         public void Execute(int i)
         {
-            hashes[i] = (uint)i;
+            float v = floor(invResolution * i + 0.00001f);
+            float u = i - resolution * v;
+            hashes[i] = (uint)(frac(u * v * 0.381f) * 256f);
         }
     }
 
@@ -39,7 +44,9 @@ public class HashVisualization : MonoBehaviour
 
         new HashJob
         {
-            hashes = hashes
+            hashes = hashes,
+            resolution = resolution,
+            invResolution = 1f / resolution
         }.ScheduleParallel(hashes.Length, resolution, default).Complete();
 
         hashesBuffer.SetData(hashes);
